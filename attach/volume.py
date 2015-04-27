84bd681260dead58abd060b90d435311f46adf2f
chenwin <c00124132@huawei.com>
2015-04-27 19:55:40 +0800

    Update volume.py

--- attach/volume.py
+++ attach/volume.py
@@ -7,14 +7,10 @@ from oslo_config import cfg
 from oslo_log import log as logging
 from oslo_utils import strutils
 
-from nova.compute import arch
 from nova import exception
 from nova.i18n import _
-from nova.i18n import _LE
-from nova.i18n import _LI
 from nova.i18n import _LW
 from nova import paths
-from nova.storage import linuxscsi
 from nova import utils
 
 保留DockerISCSIVolumeDriver、DockerBaseVolumeDriver
